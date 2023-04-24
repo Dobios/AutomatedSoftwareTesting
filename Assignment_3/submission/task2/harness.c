a4970f2ee7ebe0307f6c95dc851e1b6d4579798b
Andrew Dobis <andrew.dobis@epfl.ch>
2023-04-24 23:02:40 +0200

    WIP task 2

---
+++ Assignment_3/submission/task2/harness.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>  
+
+#include "library.h"
+
+#define SIZE 100
+#define INT_SIZE 4
+
+int main(int argc, char* argv[]) {
+    if(argc < 3) {
+        printf("Usage: %s mul|echo <input_filename>\n", argv[0]);
+    } else {
+        char buffer[SIZE] = {0};
+        // Open given file and store contents into our buffer
+        FILE *file = fopen(argv[2], "r");
+
+        // Figure out target
+        if(strcmp(argv[1], "echo") == 0) {
+            ssize_t l;
+
+            // Read the input from a file and feed to lib_echo
+            char c = getc(file);
+            int idx = 0;
+
+            // Go char by char and stop either at EOF or the buffer size
+            while (c != EOF || idx < SIZE) {
+                buffer[idx++] = c;
+                c = getc(file);
+            }
+
+            lib_echo(buffer, (ssize_t)idx /* I like being clear */);
+        } else if(strcmp(argv[1], "mul") == 0) {
+            int a, b = 0; 
+
+            // Read from the file 
+            fgets(buffer, INT_SIZE, file);
+            a = *((int*)buffer); // Amazing reinterpret cast ;)
+            fgets(buffer, INT_SIZE, file); 
+            b = *((int*)buffer);
+
+            printf("%d\n", lib_mul(a, b));
+        } else {
+            printf("Usage: %s mul|echo <input_filename>\n", argv[0]);
+        }
+        
+        fclose(file);
+    }
+
+    return 0;
+}
